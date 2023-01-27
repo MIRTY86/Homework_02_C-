374b6995aa05b729f1909e1d9e1c627a4ac8fd8d
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-27 11:02:19 +0300

    Xастотный словарь элементов двумерного массива

---
+++ Task_006/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] randomArray = FillRandomArray();
+PrintRandomArray(randomArray);
+Console.WriteLine();
+PrintResult(randomArray);
+
+
+void PrintResult(int[,] array)
+{
+    for (int i = 0; i < 11; i++)
+    {
+        int num = CountElement(i, array);
+        if(num == 2 || num == 3 || num == 4) Console.WriteLine($"{i} встречается {num} раза.");
+        else Console.WriteLine($"{i} встречается {num} раз.");
+    }
+}
+
+int[,] FillRandomArray()
+{
+
+    int[,] randomArray = new int[5, 5];
+
+    for (int y = 0; y < 5; y++)
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            randomArray[y, x] = new Random().Next(0, 11);
+        }
+    }
+    return randomArray;
+}
+
+int CountElement(int num, int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (num == array[i, j]) result++;
+        }
+    }
+    return result;
+}
+
+void PrintRandomArray(int[,] Array)
+{
+    for (int y = 0; y < randomArray.GetLength(0); y++)
+    {
+        for (int x = 0; x < randomArray.GetLength(1); x++)
+        {
+            Console.Write(randomArray[y, x] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
