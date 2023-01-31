230a7805a410959b1331c94453baf31344b19ee0
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-31 13:47:31 +0300

    Рекурсия: програма выдает все натуральные числа от N до 1.

---
+++ Task_011/Program.cs
@@ -0,0 +1,35 @@
+﻿string message1 = "Введите N :";
+int start = CheckNumber(message1);
+
+int CheckNumber(string message)
+{
+    int a = 0;
+    while (true)
+    {
+        try
+        {
+            System.Console.WriteLine(message);
+            a = int.Parse(Console.ReadLine() ?? "");
+            break;
+
+        }
+        catch (Exception exc)
+        {
+            System.Console.WriteLine($"Ошибка ввода данных!!! {exc.Message}");
+        }
+    }
+    return a;
+}
+
+Console.Write("N = " + start + " --> " + "\" ");
+DisplayNumbers(start);
+Console.WriteLine("\"");
+
+void DisplayNumbers(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n + " ");
+        DisplayNumbers(n - 1);
+    }
+}
